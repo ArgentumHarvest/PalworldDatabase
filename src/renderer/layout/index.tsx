0c8023aee6ee189a12cde400fb01d7e484e4dfc3
LiuQi <liu67iq@gmail.com>
2025-06-06 15:44:00 +0800

    调整window打包的图标

--- src/renderer/layout/index.tsx
+++ src/renderer/layout/index.tsx
@@ -4,7 +4,7 @@ import { Link, Outlet } from "react-router";
 export const Page = function Page_() {
   return (
     <div className="flex h-[100vh] overflow-hidden">
-      <div className="w-[60px] bg-orange-800">侧边栏</div>
+      <div className="w-[60px] bg-gray-800">侧边栏</div>
       <div className="flex-1 flex flex-col overflow-hidden">
         <div className="flex gap-2 bg-gray-400">
           <div className="flex-1 flex items-center overflow-hidden gap-2">
